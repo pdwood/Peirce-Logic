7c7729fac71396e21948dbbe6698f29105c546eb
Bharath Santosh <bsant99@gmail.com>
2013-02-17 19:07:35 -0500

    Moved around js files into proper directories for more organization

--- appengine/js/utils/list.js
+++ appengine/js/utils/list.js
@@ -1,96 +1,84 @@
-function ListNode(x)
-{
+function ListNode(x) {
 	this.val = x;
 	this.prev = null;
 	this.next = null;
 }
-function List()
-{
+
+function List() {
 	this.head = null;
 	this.tail = null;
 	this.length = 0;
-	for(var i = 0; i < arguments.length;i++)
-	{
+	for(var i = 0; i < arguments.length; i++) {
 		this.push_back(arguments[i]);
 	}
-};
-List.prototype.push_back = function (x)
-{
+}
+
+List.prototype.push_back = function(x) {
 	var push = new ListNode(x);
-	if(this.tail === null)
-	{
+	if(this.tail === null) {
 		this.head = this.tail = push;
-	}else{
+	} else {
 		this.tail.next = push;
 		push.prev = this.tail;
 		this.tail = push;
 	}
 	this.length++;
-}
-List.prototype.pop_back = function ()
-{
-	if(this.tail === null)
-	{
+};
+
+List.prototype.pop_back = function() {
+	if(this.tail === null) {
 		return null;
 	}
 	var ret = this.tail.val;
-	if(this.tail.prev === null)
-	{
+	if(this.tail.prev === null) {
 		this.head = null;
-	}else{
+	} else {
 		this.tail.prev.next = null;
 	}
 	this.tail = this.tail.prev;
 	this.length--;
 	return ret;
-}
-List.prototype.push_front = function (x)
-{
+};
+
+List.prototype.push_front = function(x) {
 	var push = new ListNode(x);
-	if(this.head === null)
-	{
+	if(this.head === null) {
 		this.head = this.tail = push;
-	}else{
+	} else {
 		this.head.prev = push;
 		push.next = this.head;
 		this.head = push;
 	}
 	this.length++;
-}
-List.prototype.pop_front = function ()
-{
-	if(this.head === null)
-		return null;
-	if(this.head.next === null)
-	{
+};
+
+List.prototype.pop_front = function() {
+	if(this.head === null) return null;
+	if(this.head.next === null) {
 		this.tail = null;
-	}else{
+	} else {
 		this.head.next.prev = null;
 	}
 	var ret = this.head.val;
 	this.head = this.head.next;
 	this.length--;
 	return ret;
-}
-List.prototype.erase = function(p) //returns pointer to next in list
-{
-	if(p === null)
-		return null;
-	if(p.next !== null)
-		p.next.prev = p.prev;
-	if(p.prev !== null)
-		p.prev.next = p.next;
-	if(this.head == p)
-		this.head = p.next;
-	if(this.tail == p)
-		this.tail = p.prev;
+};
+
+//returns pointer to next in list
+List.prototype.erase = function(p) {
+	if(p === null) return null;
+	if(p.next !== null) p.next.prev = p.prev;
+	if(p.prev !== null) p.prev.next = p.next;
+	if(this.head == p) this.head = p.next;
+	if(this.tail == p) this.tail = p.prev;
 	this.length--;
 	return p.next;
-}
-List.prototype.insert = function(p,x) //inserts x after p, returns where x was inserted
-{
-	if(p === null || p.next === null)
-	{
+};
+
+//inserts x after p, returns where x was inserted
+List.prototype.insert = function(p, x) {
+	if(p === null || p.next === null) {
 		this.push_back(x);
 		return this.tail;
 	}
@@ -101,97 +89,85 @@ List.prototype.insert = function(p,x) //inserts x after p, returns where x was i
 	add.prev = p;
 	this.length++;
 	return add;
-}
+};
 
-List.prototype.contains = function(value)
-{
-	var itr = this.skipUntil(function(x){return (x===value);});
-	if (itr==this.end())
-		return false;
-	else
-		return itr;
-}
+List.prototype.contains = function(value) {
+	var itr = this.skipUntil(function(x) {
+		return(x === value);
+	});
+	if(itr == this.end()) return false;
+	else return itr;
+};
 
-List.prototype.begin = function ()
-{
+List.prototype.begin = function() {
 	return this.head;
-}
-List.prototype.rbegin = function ()
-{
+};
+
+List.prototype.rbegin = function() {
 	return this.tail;
-}
-List.prototype.end = function ()
-{
+};
+
+List.prototype.end = function() {
 	return null;
-}
-List.prototype.append = function (l)
-{
-	if(!this.length)
-	{
+};
+
+List.prototype.append = function(l) {
+	if(!this.length) {
 		this.head = l.head;
 		this.tail = l.tail;
 		this.length = l.length;
 		return;
 	}
-	if(l.length)
-	{
+	if(l.length) {
 		this.tail.next = l.head;
 		l.head.prev = this.tail;
 		this.tail = l.tail;
 		this.length += l.length;
 	}
-}
+};
 
 //iterate applicator
-List.prototype.iterate = function (iterate_func)
-{
+List.prototype.iterate = function(iterate_func) {
 	var itr = this.begin();
-	while(itr!=this.end()) {
+	while(itr != this.end()) {
 		iterate_func(itr.val);
 		itr = itr.next;
 	}
-}
+};
 
 
 //iterate until condtion is true, return location or end
-List.prototype.skipUntil = function (cond_func)
-{
+List.prototype.skipUntil = function(cond_func) {
 	var itr = this.begin();
-	while(itr!=this.end()) {
-		if(cond_func(itr.val))
-			break;
+	while(itr != this.end()) {
+		if(cond_func(itr.val)) break;
 		itr = itr.next;
 	}
 	return itr;
-}
+};
 
 
 //merge sort function, pass the less then function, should presurve pointers to list (though it will change the order)
-List.prototype.sort = function (less_then_func)
-{
-	if(this.length == 0)
-		return;
+List.prototype.sort = function(less_then_func) {
+	if(this.length === 0) return;
 
 	var merge_queue = new List();
 	var itr = this.head;
 
 	//break list for merge sort and do the first step
-	while(itr != this.end())
-	{
-		if(itr.next == this.end())
-		{
+	while(itr != this.end()) {
+		if(itr.next == this.end()) {
 			itr.prev = itr.next = null;
 			merge_queue.push_back(itr);
 			break;
 		}
-		if(less_then_func(itr.val,itr.next.val))
-		{
+		if(less_then_func(itr.val, itr.next.val)) {
 			merge_queue.push_back(itr);
 			itr.prev = null;
 			var p = itr.next.next;
 			itr.next.next = null;
 			itr = p;
-		}else{
+		} else {
 			var p = itr.next.next;
 			itr.prev = itr.next;
 			itr.next.next = itr;
@@ -202,14 +178,11 @@ List.prototype.sort = function (less_then_func)
 		}
 	}
 	//merge sort the list
-	while(merge_queue.length != 1)
-	{
+	while(merge_queue.length != 1) {
 		var merge_itr = merge_queue.head;
 		var temp_queue = new List();
-		while(merge_itr != merge_queue.end())
-		{
-			if(merge_itr.next == merge_queue.end())
-			{
+		while(merge_itr != merge_queue.end()) {
+			if(merge_itr.next == merge_queue.end()) {
 				temp_queue.push_back(merge_itr.val);
 				break;
 			}
@@ -217,24 +190,21 @@ List.prototype.sort = function (less_then_func)
 			var list2_itr = merge_itr.next.val;
 			var merged_list;
 			var merged_list_itr;
-			if(less_then_func(list1_itr.val, list2_itr.val))
-			{
-				merged_list =  merged_list_itr = list1_itr;
+			if(less_then_func(list1_itr.val, list2_itr.val)) {
+				merged_list = merged_list_itr = list1_itr;
 				list1_itr = list1_itr.next;
-			}else{
+			} else {
 				merged_list = merged_list_itr = list2_itr;
 				list2_itr = list2_itr.next;
 			}
-			while(list1_itr && list2_itr)
-			{
-				if(less_then_func(list1_itr.val, list2_itr.val))
-				{
+			while(list1_itr && list2_itr) {
+				if(less_then_func(list1_itr.val, list2_itr.val)) {
 					merged_list_itr.next = list1_itr;
 					list1_itr.prev = merged_list_itr;
 					merged_list_itr = list1_itr;
 					list1_itr = list1_itr.next;
 					merged_list_itr.next = null;
-				}else{
+				} else {
 					merged_list_itr.next = list2_itr;
 					list2_itr.prev = merged_list_itr;
 					merged_list_itr = list2_itr;
@@ -242,11 +212,10 @@ List.prototype.sort = function (less_then_func)
 					merged_list_itr.next = null;
 				}
 			}
-			if(list1_itr)
-			{
+			if(list1_itr) {
 				merged_list_itr.next = list1_itr;
 				list1_itr.prev = merged_list_itr;
-			}else{
+			} else {
 				merged_list_itr.next = list2_itr;
 				list2_itr.prev = merged_list_itr;
 			}
@@ -257,6 +226,6 @@ List.prototype.sort = function (less_then_func)
 	}
 	//fix the head and tail of the sorted list
 	this.head = merge_queue.head.val;
-	for(itr = this.head;itr.next != this.end(); itr = itr.next) {}
+	for(itr = this.head; itr.next != this.end(); itr = itr.next) {}
 	this.tail = itr;
-}
+};
