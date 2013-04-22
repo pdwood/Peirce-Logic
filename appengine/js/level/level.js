58f510eb83a7cd84208e551cb0fe8548b7453d32
giganticbrain <slowit@rpi.edu>
2013-04-21 23:29:50 -0400

    Updated minimap code.

--- appengine/js/level/level.js
+++ appengine/js/level/level.js
@@ -13,9 +13,9 @@ function Level(R,parent,x,y,duplicate) {
 
 	this.shape = null;
 
-	this.DEFAULT_PLANE_WIDTH = 1000;
+	this.DEFAULT_PLANE_WIDTH = 5000;
 	this.paper.DEFAULT_PLANE_WIDTH = this.DEFAULT_PLANE_WIDTH;
-	this.DEFAULT_PLANE_HEIGHT = 200;
+	this.DEFAULT_PLANE_HEIGHT = 5000;
 	this.paper.DEFAULT_PLANE_HEIGHT = this.DEFAULT_PLANE_HEIGHT;
 	this.DEFAULT_CHILD_WIDTH = 50;
 	this.DEFAULT_CHILD_HEIGHT = 50;
@@ -196,6 +196,7 @@ current level at x,y position (x,y is center)
 returns child
 */
 Level.prototype.addChild = function(x,y) {
+	minimap.addPoint(x, y);
 	var child = new Level(this.paper,this,x-this.DEFAULT_CHILD_WIDTH/2,y-this.DEFAULT_CHILD_HEIGHT/2);
 	child.shape.toFront();
 	this.subtrees.push_back(child);
@@ -208,7 +209,7 @@ Level.prototype.addChild = function(x,y) {
 
 
 /*
-Level.addChild
+Level.addVariable
 ~x: new variable x
 ~y: new variable y
 
