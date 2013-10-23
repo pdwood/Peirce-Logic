f0c9d15649c79136cd279cc5c18f47389b77c4ba
Dimitar Dimitrov <newbrict@gmail.com>
2013-10-23 08:40:39 -0400

    fake json gen

---
+++ appengine/js/proof/fake_json.js
@@ -0,0 +1,12 @@
+fake_json = function() {
+	var someJson = {
+		"this":"is",
+ 	 "some":"fake",
+ 	 "js-n":{
+			"string":"that",
+			"bharath":"told"
+ 	 },
+		"me":"tomake"
+  };
+	D(someJson);
+}();
