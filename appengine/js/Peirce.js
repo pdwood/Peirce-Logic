f93e2b64ef4340b6fddc4abc355b1f571d4dc1d0
David Kim <davekim94@gmail.com>
2014-04-16 17:50:54 -0400

    Made goal button more efficient.

--- appengine/js/Peirce.js
+++ appengine/js/Peirce.js
@@ -73,9 +73,11 @@ window.onload = function() {
 	$('#goalbutton').click(function() {
 		if ($(this).attr('value') == 'goGoal') {
 			backNode = TheProof.current;
-			while ( TheProof.current.mode !== Proof.LOGIC_MODES.GOAL_MODE ) {
-				TheProof.prev();
+			node = TheProof.current;
+			while ( node.mode !== Proof.LOGIC_MODES.GOAL_MODE ) {
+				node = node.prev;
 			}
+			TheProof.select(node);
 			this.disabled = false;
 			this.innerHTML = 'Go Back';
 			$(this).attr('value', 'goBack');
