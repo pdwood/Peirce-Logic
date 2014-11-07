3b60f1aade0b7976889bb6b9d1275835f4a7019c
Dimitar Dimitrov <newbrict@gmail.com>
2014-11-07 11:15:28 -0500

    working on touch stuff, some preliminary things

--- appengine/js/touch/touch.js
+++ appengine/js/touch/touch.js
@@ -1,12 +1,48 @@
-document.addEventListener("touchend", handlerFunction, false);
-
 var mc;
 
 function initHammer() {
-	mc = new Hammer(document.getElementById("paper"));
+	// Paper
+	(function(){
+		mc = new Hammer.Manager(document.getElementById("paper"));
+		mc.add( new Hammer.Tap({ event: "doubletap", taps: 2 }));
+		mc.on("doubletap", touchContext);
+	})();
+
+	// Save Button
+	(function(){
+		mc = new Hammer(document.getElementById("saveButton"));
+		mc.on("tap", touchSave);
+	})();
+
+	// Load Button
+	(function(){
+		mc = new Hammer(document.getElementById("loadButton"));
+		mc.on("tap", touchLoad);
+	})();
+
+	// New Button
+	(function(){
+		mc = new Hammer(document.getElementById("newButton"));
+		mc.on("tap", touchNew);
+	})();
 }
 
-function handlerFunction(event) {
-	D("touch");
+function touchContext(ev) {
+	D("Open up context menu");
+	//root = TheProof.current.nodeTree
+	//D(root)
+	//ContextMenu.SingleClickHandler(root, 50, 50, {type:"dblclick"})
+	//event.initMouseEvent({type:"dblclick", screenX:50, screenY:50});
 }
 
+function touchSave(ev) {
+	$('#saveButton').click()
+}
+
+function touchLoad(ev) {
+	$('#loadButton').click()
+}
+
+function touchNew(ev) {
+	$('#newButton').click()
+}
